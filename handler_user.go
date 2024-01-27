f70e1519887a25d34f4149416a843a31a5f605ff
Fernando Luz <prof.fernando.luz@gmail.com>
2024-01-27 00:08:45 +0000

    feat(endpoint): add GET users using API Key in Authorization header

--- handler_user.go
+++ handler_user.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/fluz/go-rss/internal/auth"
 	"github.com/fluz/go-rss/internal/database"
 	"github.com/google/uuid"
 )
@@ -34,5 +35,22 @@ func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Reques
 		return
 	}
 
+	respondWithJSON(w, 201, databaseUserToUser(user))
+}
+
+func (apiCfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request) {
+	apiKey, err := auth.GetAPIKey(r.Header)
+	if err != nil {
+		respondWithError(w, 403, fmt.Sprintf("Auth error: %v", err))
+		return
+	}
+
+	user, err := apiCfg.DB.GetUserByAPIKey(r.Context(), apiKey)
+	if err != nil {
+		respondWithError(w, 404, fmt.Sprintf("Couldn't get user: %v", err))
+		return
+	}
+
 	respondWithJSON(w, 200, databaseUserToUser(user))
+
 }
