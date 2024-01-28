dc4a05be33aa60816896a873dda720c7a06fbad5
Fernando Luz <prof.fernando.luz@gmail.com>
2024-01-28 20:36:44 +0000

    feat(endpoint): Add DEL /feed_follows/{ID} endpoint

--- handler_feed_follows.go
+++ handler_feed_follows.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/fluz/go-rss/internal/database"
+	"github.com/go-chi/chi"
 	"github.com/google/uuid"
 )
 
@@ -46,4 +47,25 @@ func (apiCfg *apiConfig) handlerGetFeedFollows(w http.ResponseWriter, r *http.Re
 	}
 
 	respondWithJSON(w, 200, databaseFeedFollowsToFeedFollows(feedFollows))
+}
+
+func (apiCfg *apiConfig) handlerDeleteFeedFollow(w http.ResponseWriter, r *http.Request, user database.User) {
+	feedFollowIDStr := chi.URLParam(r, "feedFollowID")
+
+	feedFollowID, err := uuid.Parse(feedFollowIDStr)
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Couldn't parse feed follow id: %v", err))
+		return
+	}
+
+	err = apiCfg.DB.DeleteFeedFollow(r.Context(), database.DeleteFeedFollowParams{
+		ID:     feedFollowID,
+		UserID: user.ID,
+	})
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Couldn't delete feed follows: %v", err))
+		return
+	}
+
+	respondWithJSON(w, 204, struct{}{})
 }
