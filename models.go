7835650328532d814e8841f10868dc99e79dddfb
Fernando Luz <prof.fernando.luz@gmail.com>
2024-01-28 15:58:15 +0000

    feat(endpoint): add GET and POST for /feeds endpoint

--- models.go
+++ models.go
@@ -12,7 +12,7 @@ type User struct {
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 	Name      string    `json:"name"`
-	ApiKey    string    `json:"api_key`
+	ApiKey    string    `json:"api_key"`
 }
 
 func databaseUserToUser(dbUser database.User) User {
@@ -23,4 +23,34 @@ func databaseUserToUser(dbUser database.User) User {
 		Name:      dbUser.Name,
 		ApiKey:    dbUser.ApiKey,
 	}
+}
+
+type Feed struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Name      string    `json:"name"`
+	Url       string    `json:"url"`
+	UserID    uuid.UUID `json:"user_id"`
+}
+
+func databaseFeedToFeed(dbFeed database.Feed) Feed {
+	return Feed{
+		ID:        dbFeed.ID,
+		CreatedAt: dbFeed.CreatedAt,
+		UpdatedAt: dbFeed.UpdatedAt,
+		Name:      dbFeed.Name,
+		Url:       dbFeed.Url,
+		UserID:    dbFeed.UserID,
+	}
+}
+
+func databaseFeedsToFeeds(dbFeed []database.Feed) []Feed {
+	feeds := []Feed{}
+
+	for _, dbFeed := range dbFeed {
+		feeds = append(feeds, databaseFeedToFeed(dbFeed))
+	}
+
+	return feeds
 }
